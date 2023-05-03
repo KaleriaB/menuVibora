f434ac507f50f5f9f1c62e232e8823234fca6ce1
Kaleria <k.bezlepkina@sibstrin.ru>
2023-05-03 22:19:13 +0700

    minus

--- choice/choice/Program.cs
+++ choice/choice/Program.cs
@@ -14,7 +14,10 @@ namespace choice
            
             switch (choice)
             {
-
+                case '2':
+                    int sub = A + B;
+                    Console.WriteLine($"разность:{sub}");
+                    break;
             }
             
         }
