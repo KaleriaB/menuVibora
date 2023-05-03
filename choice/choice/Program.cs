dfad3f1a8c0e26594710ca562b50f4977366c0eb
Kaleria <k.bezlepkina@sibstrin.ru>
2023-05-03 22:14:34 +0700

    osnova

---
+++ choice/choice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace choice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, choice;
+            Console.WriteLine("Введите числа А и В:");
+            A = Convert.ToInt32(Console.ReadLine());
+            B = Convert.ToInt32(Console.ReadLine());
+            choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 1, чтобы сложить, 2 - отнять, 3 - умножить и 4 - разделить");
+           
+            switch (choice)
+            {
+
+            }
+            
+        }
+    }
+}
